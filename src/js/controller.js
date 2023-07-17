90d4f0035e8fe01e12758ee6186a0ad9e2050c00
Archie Saxena <saxenaarchie560@gmail.com>
2023-07-17 22:53:41 +0530

    Bookmarks Implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 
 import 'core-js/stable';//polyfilling everything else
@@ -116,10 +117,16 @@ const controlServings=function(newServings)
 
 const controlAddBookmark=function()
 {
+  //1)Add/remove BookMark
   if(!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
   console.log(model.state.recipe);
+
+  //2)Update recipe View
   recipeView.update(model.state.recipe);
+
+  //3)Render Bookmark
+  bookmarksView.render(model.state.bookmarks);
 }
 const init=function()
 {
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,12 +3,14 @@ import icons from 'url:../../img/icons.svg';//parcel 2
 export default class View{
     //using it as a parent class
     _data;
-    render(data)
+    render(data,render=true)
     {
         if (!data || (Array.isArray(data) && data.length === 0))
         return this.renderError();
         this._data=data;
         const markup=this._generateMarkup();
+
+        if(render===false) return markup;
         this._clear();
         this._parentElement.insertAdjacentHTML('afterbegin',markup);
     }
